cc87de0e56d66b53b3f717a4c03479ae93577736
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-02 15:32:27 +0300

    Update Header.styled.jsx

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 export const MainHeader = styled.h1`
-margin-bottom: ${props => props.theme.space[4]}px;
-text-align: center;
-  color: ${props => props.theme.color.accent}
+  margin-bottom: ${props => props.theme.space[4]}px;
+  text-align: center;
+  color: ${props => {
+    console.log(props.theme.colors.accent);
+    return props.theme.colors.accent;
+  }}
   text-shadow: ${props => props.theme.shadows.textShadow}
-  font-size: ${props => props.theme.fontSize.l};
+  font-size: ${props => props.theme.fontSizes.l};
 `;
